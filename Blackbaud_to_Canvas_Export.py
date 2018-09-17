4e8c69628f66b21917ed45c60740f3300355a89f
Justin Dammeier <31460213+jbdammeier@users.noreply.github.com>
2018-09-17 16:07:08 -0400

    Update Blackbaud_to_Canvas_Export.py

--- Blackbaud_to_Canvas_Export.py
+++ Blackbaud_to_Canvas_Export.py
@@ -5,7 +5,7 @@
 #                                                                                         #                                                                              
 # Author:      Justin Dammeier                                                            #
 #                                                                                         #
-# Created:     9/16/2018                                                                 #
+# Created:     8/16/2017                                                                  #
 # Copyright:   (c) jdammeier 2018                                                         #
 #-----------------------------------------------------------------------------------------#
 
@@ -49,7 +49,7 @@ def getInput(file_in):
 
 def get_bb_file(list_id, file_out):
       '''Converts JSON formatted data from Blackbaud LIST API into csv file. Provde onProduct list ID and final filename'''
-      url = api_enpoint + 'api/list/' + list_id + '/?t=' + token
+      url = api_endpoint + 'list/' + list_id + '/?t=' + token
       response = urlopen(url)
       data = json.loads(response.read().decode('utf-8'))
 
@@ -67,8 +67,8 @@ def get_bb_file(list_id, file_out):
 def course_merge(file_in, file_out, subaccount_id):
       '''Produces Temp CSV for courses and adds the correct SchoolID to the file'''
       outFile = open(file_out, 'w')
-      lines = getInput(file_in) #path to input file
-      
+      lines = getInput(file_in)
+
       #define & write header row for final CSV
       header = ['CourseID','CourseTitle','SchoolLevel','SchoolID']
       columnHead = ','.join(header)
@@ -80,17 +80,12 @@ def course_merge(file_in, file_out, subaccount_id):
             result = ','.join(final)
             outFile.write(result + '\n')
 
-      outFile.close()
-
-def ftp_upload(local_path, remote_path):
+def ftp_upload(local_path, remote_path, host, username, password):
       #open host
-      host = 'kimono.exavault.com' #kimono sftp server or sftp address from Canvas
       port = 22
-      transport = paramiko.Transport((host, port))
+      transport = paramiko.Transport((host, port)) 
 
-      #authenticate with username and password obtained from Kimono or Canvas
-      username = 'kimono_sftp_username'
-      password = 'kimono_sftp_password'
+      #authenticate
       transport.connect(username = username, password = password)
 
       #go
@@ -105,8 +100,7 @@ def ftp_upload(local_path, remote_path):
 
 def get_contacts(filename):
       """
-      Return two lists names, emails containing names and email addresses
-      read from a file specified by filename.
+      Return two lists names, emails containing names and email addresses read from a file specified by filename.
       """
       names = []
       emails = []
@@ -121,56 +115,70 @@ def read_template(filename):
       """
       Returns a Template object comprising the contents of the file specified by filename.
       """
-    
       with open(filename, 'r', encoding='utf-8') as template_file:
             template_file_content = template_file.read()
       return Template(template_file_content)
 
-def send_email(username, password, subject, contacts, message):
-      """
-      Sends email via google smtp server. Limited to approximately 90 emails per run.
-      """
-      names, emails = get_contacts(contacts) #read contacts
-      message_template = read_template(message) # get message body
-      
-      # set up the SMTP (this example uses gmail)
+def send_email(email_from, username, password, subject, contacts, body):
+      names, emails = get_contacts(contacts) # read contacts
+      message_template = read_template(body) # read template
+
+      # set up the SMTP server
       s = smtplib.SMTP(host='smtp.gmail.com', port=587)
       s.starttls()
       s.login(username, password)
-      
+
       # For each contact, send the email:
       for name, email in zip(names, emails):
             msg = MIMEMultipart()       # create a message
-            
+
             # add in the actual person name to the message template
             message = message_template.substitute(PERSON_NAME=name.title(), SUBJECT=subject)
-            
+
             # setup the parameters of the message
+            msg['From']=email_from
             msg['To']=email
             msg['Subject']=subject
-            
+
             # add in the message body
             msg.attach(MIMEText(message, 'plain'))
-            
-            # send the message via the smpt connection set up earlier and then deletes the message.
+
+            # send the message via the server set up earlier.
             s.send_message(msg)
-            
-            #comment out next line to keep message in the sent folder
             del msg
-        
+
       # Terminate the SMTP session and close the connection
       s.quit()
 
-#-------------------------------------------------------------------------------#
-# main program - only edit content below.                                       #
-#-------------------------------------------------------------------------------#
+######################################################################################################################
+# MAIN PROGRAM - ONLY EDIT AFTER THIS POINT                                                                          #
+######################################################################################################################
 
-#define global variables
-token = get_bb_token('username','password') #first parameter is username and second is password of user account with WEB API role
-api_endpoint = 'https://#yourschool#.myschoolapp.com/api/' #sets first part of api urls throughout the program
+#--------------------------------------------------Global Variables--------------------------------------------------#
+api_endpoint = 'https://#Yourschool#.myschoolapp.com/api/' #Sets first part of api urls throughout the program
 
-#Build files from Blackbaud onProducts
-#First paremeter is Blackbaud advanced list id, second is Path to output file
+#Blackbaud ON user account credentials with WEB API role
+bb_user = 'XXXXXXXXXX'
+bb_user_pw = 'XXXXXXXXXX'
+
+#FTP Account authentication variables & host from Canvas
+ftp_host = 'enter.host.here'
+ftp_user = 'XXXXXXXXXX'
+ftp_pw = 'XXXXXXXXXX'
+
+#Email account credentials & variables
+email_from = 'Populates From Field'
+gmail_address = 'username@domain.org'
+email_user_pw = 'XXXXXXXXXX'
+email_subject = 'Populates Subject Field'
+
+#---------------------------------------------------Program Steps---------------------------------------------------#
+
+#Step 1 = Get Token
+token = get_bb_token(bb_user,bb_user_pw) #first parameter is username and second is password of user account with WEB API role
+
+#Step 2 = Use ON API to pull advanced list data
+#First parameter is Blackbaud advanced list id, second is Path to output file
 get_bb_file('00000','C:\Path_to_output_file\STAFF.csv')
 get_bb_file('00000','C:\Path_to_output_filefile\STUDENT.csv')
 get_bb_file('00000','C:\Path_to_output_filefile\TERM.csv')
@@ -178,12 +186,12 @@ get_bb_file('00000','C:\Path_to_output_filefile\COURSE_TEMP.csv')
 get_bb_file('00000','C:\Path_to_output_filefile\COURSE_SECTION.csv')
 get_bb_file('00000','C:\Path_to_output_filefile\STUDENT_COURSE_SECTION_ENROLLMENT.csv')
 
-#Edit course files
-#First parameter is Path to input file, second is path to output file, third is Canvas SIS ID for the subaccount the courses will go into
+#Step 3 = Edit course files.
+#Takes activity input file, course input file, output filename, activity subaccount ID, and course subaccount ID as arguments
 course_merge('C:\Path_to_input_file\COURSE_TEMP.csv', 'C:\Path_to_output_file\COURSE.csv', '3')
 
-#Send files to Kimono via sftp, comment out to test program before sending files
-#only edit first parameter path. filenames should not be edited.
+#Step 4 = Send FINAL files to Kimono via sftp.
+#Only edit first parameter path. filenames should not be edited.
 ftp_upload('C:\Path_to_input_file\STAFF.csv', '/STAFF.csv')
 ftp_upload('C:\Path_to_input_file\STUDENT.csv', '/STUDENT.csv')
 ftp_upload('C:\Path_to_input_file\TERM.csv', '/TERM.csv')
@@ -191,6 +199,6 @@ ftp_upload('C:\Path_to_input_file\COURSE.csv', '/COURSE.csv')
 ftp_upload('C:\Path_to_input_file\COURSE_SECTION.csv', '/COURSE_SECTION.csv')
 ftp_upload('C:\Path_to_input_file\STUDENT_COURSE_SECTION_ENROLLMENT.csv', '/STUDENT_COURSE_SECTION_ENROLLMENT.csv')
 
-#Send email confirmations
+#Step 5 = Send email confirmations
 #First parameter is email address, Second is Password, Third is Subject, Fourth is contact list, Fifth is message body
-send_email('gmail_address', 'password', 'Subject', 'C:\Path_to_file\mycontacts.txt', 'C:\Path_to_file\message.txt')
+send_email(email_from, gmail_address, email_user_pw, email_subject, 'C:\Path_to_file\mycontacts.txt', 'C:\Path_to_file\message.txt')
